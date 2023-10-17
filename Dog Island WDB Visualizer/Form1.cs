b41b442701fb5710f9c89b3baaf10e2e3d195659
Matthew Cacutt <mcacutt@hotmail.com>
2023-10-17 14:51:33 +0100

    .wds and Japanese encoding support

--- Dog Island WDB Visualizer/Form1.cs
+++ Dog Island WDB Visualizer/Form1.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace Dog_Island_WDB_Visualizer
 {
